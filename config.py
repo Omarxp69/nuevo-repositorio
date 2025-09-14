b52b7c8b409b4e77a39a4f461ea8bf811e524b4f
Omarxp69 <135158823+Omarxp69@users.noreply.github.com>
2025-09-13 21:41:14 -0600

    Actualizar config.py para usar host remoto de Railway

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'clave_local')
-    DB_HOST = os.environ.get('MYSQLHOST')          # debe ser ${RAILWAY_PRIVATE_DOMAIN}
-    DB_USER = os.environ.get('MYSQLUSER')          # root
-    DB_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')  # gXzKWOUKgRfhZPechGJvDRfjikESKknh
-    DB_NAME = os.environ.get('MYSQL_DATABASE')     # railway
+    DB_HOST = os.environ.get('MYSQLHOST')          # no 'localhost'
+    DB_USER = os.environ.get('MYSQLUSER')          # normalmente 'root'
+    DB_PASSWORD = os.environ.get('MYSQL_ROOT_PASSWORD')
+    DB_NAME = os.environ.get('MYSQL_DATABASE')     # 'railway'
     DB_PORT = int(os.environ.get('MYSQLPORT', 3306))
